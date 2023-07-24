1acdcdf74106de50eae00869ad5e97e3d67f0e80
JustSemen <semyn03@mail.ru>
2023-07-24 11:45:00 +0300

    fix tsconfig for exclude test in dist

--- src/settings.ts
+++ src/settings.ts
@@ -10,14 +10,9 @@ export const baseURL = '/api/'
 
 export const HTTP_STATUSES = {
     OK_200: 200,
-    CREATED_201: 201,
-    NO_CONTENT_204: 204,
 
     NOT_FOUND_404: 404,
     BAD_REQUEST_400: 400,
-    UNAUTHORIZED_401: 401,
-    FORBIDDEN_403: 403,
-    TOO_MANY_REQUESTS_429: 429,
 
     NOT_IMPLEMENTED_501: 501
 }
